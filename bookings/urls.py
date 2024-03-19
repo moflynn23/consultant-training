53910bfb52dcb095f0103a7cb859d479f510811f
Mark O'Flynn <mark.oflynn1@gmail.com>
2024-03-19 23:09:01 +0000

    Delete bookings

--- bookings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AddBooking, Bookings
-
-
-urlpatterns = [
-    path("", AddBooking.as_view(), name="add_booking"),
-    path("bookings/", Bookings.as_view(), name="bookings"),
-]
